04621f1832de8ef98c5382fca172b920a870121d
T <taesukim@skiff.com>
2025-03-22 22:32:40 +0000

    Create hathi01.js

---
+++ hathi/hathi01.js
@@ -0,0 +1,3 @@
+{
+  "ads": "Join the discord that no one ares about: <a href=\"https://discord.com/hati01\" target=\"_blank\" style=\"text-decoration: underline;\">https://discord.com/hati01</a>. Hi, you can display your hathi ass. For donation for hathi, please contact the zoo: <a href=\"https://t.me/ragu2k\" target=\"_blank\" style=\"text-decoration: underline;\">Contact me</a>"
+}
